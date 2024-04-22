61aa15f7c57fc740d9c031d71cf79a8c88984dc5
yoniu <1376652515@qq.com>
2024-04-22 17:34:05 +0800

    feat: 无网络页 UI

--- src/views/offline/Index.vue
+++ src/views/offline/Index.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>offline</div>
+  <a-result status="error" title="No internet ">
+    <template #icon>
+      <IconFaceFrownFill />
+    </template>
+    <template #subtitle> DNS_PROBE_FINISHED_NO_INTERNET </template>
+    <template #extra>
+      <a-button type="primary">Refresh</a-button>
+    </template>
+    <a-typography style="background: var(--color-fill-2); padding: 24px">
+      <a-typography-paragraph>Try:</a-typography-paragraph>
+      <ul>
+        <li>Checking the network cables, modem, and router</li>
+        <li>Reconnecting to Wi-Fi</li>
+      </ul>
+    </a-typography>
+  </a-result>
 </template>
+
+<style lang="less">
+#app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+</style>
